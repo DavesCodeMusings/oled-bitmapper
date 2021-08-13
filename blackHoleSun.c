ff119983bc77ea6153964bd840725041079dff5b
Dave <61114342+DavesCodeMusings@users.noreply.github.com>
2021-08-13 17:07:25 -0500

    Delete blackHoleSun.c

--- blackHoleSun.c
+++
@@ -1,2 +0,0 @@
-// 16px high by 16px wide bitmap of a black hole. To wash away the rain.
-unsigned char blackHoleSun[] = {199,192,159,240,56,24,96,12,64,4,192,6,192,6,224,6,224,6,240,14,120,60,127,252,63,248,31,242,7,198,0,0}; 
